4136c685423ee236576d44ce3d1e281ab524ef9b
Samuel Oliveira <renard162@gmail.com>
2026-08-13 14:44:19 -0300

    Rollback do hook do cshell

--- src/uv_global_wrapper/common/hooks/templates/cshell.py
+++ src/uv_global_wrapper/common/hooks/templates/cshell.py
@@ -11,15 +11,6 @@ def template_cshell_hook_script() -> str:
     commands = " ".join(COMMANDS_DICT.values())
     venvs_path = path_as_posix(venvs_root_path(abs_path=False))
 
-    return dedent(
-        f"""
-        alias uve 'if ("!:1" == "activate" && "!:2" != "" && "!:2" != "-h" && "!:2" != "--help") then; eval `\\uve activate "\\!:2" --hook cshell`; else; \\uve !\\*; endif'
-
-        if ($?tcsh) then
-            complete uve \\
-                'p/1/({commands})/' \\
-                'n/activate/`find "$HOME/{venvs_path}" -mindepth 1 -maxdepth 1 -type d -exec basename {{}} \\\\;/`/' \\
-                'n/delete/`find "$HOME/{venvs_path}" -mindepth 1 -maxdepth 1 -type d -exec basename {{}} \\\\;/`/'
-        endif
-        """
-    ).strip()
+    return dedent(r"""
+        alias uvg 'if ("!:1" == "activate" && "!:2" != "" && "!:2" != "-h" && "!:2" != "--help") then; eval `\uvg activate "\!:2" --hook cshell`; else; \uvg !\*; endif'
+    """).strip()
