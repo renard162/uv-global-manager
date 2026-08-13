f5f34c10883801de8cd902deb6fee48809e32234
Samuel Oliveira <renard162@gmail.com>
2026-08-13 02:49:33 -0300

    Implemented autocomplete on XonSH

--- src/uv_global_wrapper/common/hooks/templates/xonsh.py
+++ src/uv_global_wrapper/common/hooks/templates/xonsh.py
@@ -1,10 +1,52 @@
 from textwrap import dedent
 
+from ....commands import COMMANDS_DICT
+from ...paths import (
+    path_as_posix,
+    venvs_root_path,
+)
+
 
 def template_xonsh_hook_script() -> str:
-    return dedent("""
+    commands = repr(tuple(COMMANDS_DICT.values()))
+    venvs_path = path_as_posix(venvs_root_path(abs_path=False))
+
+    return dedent(f"""
+        import os
         import subprocess
         import sys
+
+        _uve_commands = {commands}
+        _uve_venvs_root = os.path.expanduser("~/{venvs_path}")
+
+        def _uve_complete(command, alias):
+            if command.arg_index == 1:
+                prefix = command.prefix
+
+                return {{
+                    option
+                    for option in _uve_commands
+                    if option.startswith(prefix)
+                }}
+
+            if command.arg_index != 2:
+                return None
+
+            subcommand = command.args[1].value
+
+            if subcommand not in ("activate", "delete"):
+                return None
+
+            if not os.path.isdir(_uve_venvs_root):
+                return set()
+
+            prefix = command.prefix
+
+            return {{
+                entry.name
+                for entry in os.scandir(_uve_venvs_root)
+                if entry.is_dir() and entry.name.startswith(prefix)
+            }}
 
         def _uve(args):
             if (
@@ -27,5 +69,6 @@ def template_xonsh_hook_script() -> str:
 
             return subprocess.run(["uve", *args]).returncode
 
+        _uve.xonsh_complete = _uve_complete
         __xonsh__.aliases["uve"] = _uve
     """).strip()
