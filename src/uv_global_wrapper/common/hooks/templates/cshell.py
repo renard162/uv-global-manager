1b4b33033d6ff6d6aebb53a52d378615e41c55b3
Samuel Oliveira <renard162@gmail.com>
2026-08-13 15:57:44 -0300

    Small fix in cshell hook syntax

--- src/uv_global_wrapper/common/hooks/templates/cshell.py
+++ src/uv_global_wrapper/common/hooks/templates/cshell.py
@@ -12,5 +12,5 @@ def template_cshell_hook_script() -> str:
     venvs_path = path_as_posix(venvs_root_path(abs_path=False))
 
     return dedent(r"""
-        alias uvg 'if ("!:1" == "activate" && "!:2" != "" && "!:2" != "-h" && "!:2" != "--help") then; eval `\uvg activate "\!:2" --hook cshell`; else; \uvg !\*; endif'
+        alias uvg 'if ("\!:1" == "activate" && "\!:2" != "" && "\!:2" != "-h" && "\!:2" != "--help") then; eval `\uvg activate "\!:2" --hook cshell`; else; \uvg \!*; endif'
     """).strip()
