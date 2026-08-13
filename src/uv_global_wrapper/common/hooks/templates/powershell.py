ce101f6afe78f74737fd16fa886c2628eed31ffc
Samuel Oliveira <renard162@gmail.com>
2026-08-13 02:39:06 -0300

    Implemented powershell autocomplete

--- src/uv_global_wrapper/common/hooks/templates/powershell.py
+++ src/uv_global_wrapper/common/hooks/templates/powershell.py
@@ -1,27 +1,94 @@
 from textwrap import dedent
 
+from ....commands import COMMANDS_DICT
+from ...paths import (
+    path_as_windows,
+    venvs_root_path,
+)
+
 
 def template_powershell_hook_script() -> str:
-    return dedent("""
+    commands = " ".join(COMMANDS_DICT.values())
+    venvs_path = path_as_windows(venvs_root_path(abs_path=False))
+
+    return dedent(f"""
         $uve_command = (Get-Command uve -CommandType Application).Source
 
-        function uve {
-            if (
-                $args.Count -ge 2 -and
-                $args[0] -eq "activate" -and
-                $args[1] -ne "-h" -and
-                $args[1] -ne "--help"
-            ) {
-                $activation_command = & $uve_command activate $args[1] --hook powershell
+        function uve {{
+            param(
+                [Parameter(ValueFromRemainingArguments)]
+                [ArgumentCompleter({{
+                    param(
+                        $commandName,
+                        $parameterName,
+                        $wordToComplete,
+                        $commandAst,
+                        $fakeBoundParameters
+                    )
 
-                if ($LASTEXITCODE -ne 0) {
+                    $elements = @($commandAst.CommandElements)
+                    $arguments = @(
+                        $elements |
+                            Select-Object -Skip 1 |
+                            ForEach-Object Value
+                    )
+
+                    if ($arguments.Count -eq 0) {{
+                        "{commands}".Split(" ") |
+                            Where-Object {{ $_ -like "$wordToComplete*" }} |
+                            ForEach-Object {{
+                                [System.Management.Automation.CompletionResult]::new(
+                                    $_,
+                                    $_,
+                                    [System.Management.Automation.CompletionResultType]::ParameterValue,
+                                    $_
+                                )
+                            }}
+
+                        return
+                    }}
+
+                    if (
+                        $arguments.Count -ge 1 -and
+                        $arguments[0] -in @("activate", "delete")
+                    ) {{
+                        $venvsRoot = Join-Path $HOME "{venvs_path}"
+
+                        if (-not (Test-Path -LiteralPath $venvsRoot -PathType Container)) {{
+                            return
+                        }}
+
+                        Get-ChildItem -LiteralPath $venvsRoot -Directory |
+                            Where-Object {{ $_.Name -like "$wordToComplete*" }} |
+                            ForEach-Object {{
+                                [System.Management.Automation.CompletionResult]::new(
+                                    $_.Name,
+                                    $_.Name,
+                                    [System.Management.Automation.CompletionResultType]::ParameterValue,
+                                    $_.Name
+                                )
+                            }}
+                    }}
+                }})]
+                [string[]]$Arguments
+            )
+
+            if (
+                $Arguments.Count -ge 2 -and
+                $Arguments[0] -eq "activate" -and
+                $Arguments[1] -ne "-h" -and
+                $Arguments[1] -ne "--help"
+            ) {{
+                $activation_command = & $uve_command activate $Arguments[1] --hook powershell
+
+                if ($LASTEXITCODE -ne 0) {{
                     return
-                }
+                }}
 
                 Invoke-Expression $activation_command
                 return
-            }
+            }}
 
-            & $uve_command @args
-        }
+            & $uve_command @Arguments
+        }}
     """).strip()
